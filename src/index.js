e76eedd62d5f18ec97d635b0a732fc1f885a93f1
Taylor Otwell <taylor@laravel.com>
2019-02-28 16:20:34 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ const axios = require('axios')
 
 class Vapor
 {
-    work() {
-        axios.get('/foo')
-            .then(response => {
-                console.log(response.data);
-            })
+    store() {
+        //
     }
 }
 
