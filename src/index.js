2a5f46793adf52ecd3e98d0353fcc9fee33ded2c
clmntgr <clement@wisepops.com>
2019-12-16 16:58:02 +0100

    Fix calling store method without options
    Pass an empty object instead of null as default argument for options

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class Vapor
     /**
      * Store a file in S3 and return its UUID, key, and other information.
      */
-    async store(file, options = null) {
+    async store(file, options = {}) {
         const response = await axios.post('/vapor/signed-storage-url', {
             'bucket': options.bucket || '',
             'content_type': options.contentType || file.type,
