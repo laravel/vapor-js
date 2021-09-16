34b9da1727e471c8c23c258a7100b82fc29cc5ac
Kieron Wiltshire <KieronWiltshire@users.noreply.github.com>
2021-09-16 11:01:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Vapor
      * Store a file in S3 and return its UUID, key, and other information.
      */
     async store(file, options = {}) {
-        const response = await axios.post('/vapor/signed-storage-url', {
+        const response = await axios.post(`${options.baseUrl}/vapor/signed-storage-url`, {
             'bucket': options.bucket || '',
             'content_type': options.contentType || file.type,
             'expires': options.expires || '',
