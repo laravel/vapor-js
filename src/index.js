4c26e5cbe4b259482eaafcc7f0f8943b5c2b18bd
rupam <rupam@claritytech.io>
2021-10-27 13:03:22 +0530

    custom signed storage url

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class Vapor
      * Store a file in S3 and return its UUID, key, and other information.
      */
     async store(file, options = {}) {
-        const response = await axios.post('/vapor/signed-storage-url', {
+        const response = await axios.post(options.signedStorageUrl ? options.signedStorageUrl : '/vapor/signed-storage-url', {
             'bucket': options.bucket || '',
             'content_type': options.contentType || file.type,
             'expires': options.expires || '',
